5ccea67a6d8292157a765189ddffa09610d56d5e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-10-30 11:50:36 +0630

    Add project files.

---
+++ QuartzNetJobMvc/Controllers/AnnouncementController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuartzNetJobMvc.Controllers
+{
+    // Controllers/AnnouncementController.cs
+    public class AnnouncementController : Controller
+    {
+        private readonly YourDbContext _dbContext;
+
+        public AnnouncementController(YourDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Announcements.Add(announcement);
+                await _dbContext.SaveChangesAsync();
+                // Trigger the email job manually or it will be picked up by the job’s schedule
+                return RedirectToAction("Index", "Home");
+            }
+            return View(announcement);
+        }
+    }
+}
---
+++ QuartzNetJobMvc/Controllers/ServiceController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuartzNetJobMvc.Controllers
+{
+    // Controllers/ServiceController.cs
+    public class ServiceController : Controller
+    {
+        private readonly YourDbContext _dbContext;
+
+        public ServiceController(YourDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var emailServiceSetting = await _dbContext.ServiceSettings
+                .FirstOrDefaultAsync(s => s.ServiceName == "EmailService");
+
+            return View(emailServiceSetting);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleEmailService()
+        {
+            var emailServiceSetting = await _dbContext.ServiceSettings
+                .FirstOrDefaultAsync(s => s.ServiceName == "EmailService");
+
+            if (emailServiceSetting != null)
+            {
+                emailServiceSetting.IsEnabled = !emailServiceSetting.IsEnabled;
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
